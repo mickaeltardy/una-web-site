3bde4a39a7087d302da3dbd4b6ae89d921cced9d
MaximeMoro <maxime.moro76@gmail.com>
2014-04-01 15:38:40 +0200

    #1 I just added commentaries to explain methods

--- Articles/app/models/Article.java
+++ Articles/app/models/Article.java
@@ -20,14 +20,17 @@ public class Article{
 	
 	private static JacksonDBCollection<Article, String> coll = MongoDB.getCollection("articles", Article.class, String.class);
 
+	// Method to collect all articles from DataBase
 	public static List<Article> all() {
 	    return coll.find().toArray();
 	}
 
+	// Method to add an article to the DataBase
 	public static void create(Article article) {
 		coll.save(article);
 	}
 
+	// Method to delete an article from the DataBase
 	public static void delete(String id) {
 	    Article article = Article.coll.findOneById(id);
 	    if (article != null)
