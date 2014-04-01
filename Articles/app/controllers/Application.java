3cf47e2e393c9ca9f60ac122af29f5a443a81674
MaximeMoro <maxime.moro76@gmail.com>
2014-04-01 20:26:50 +0200

    #6 Still in process

--- Articles/app/controllers/Application.java
+++ Articles/app/controllers/Application.java
@@ -3,9 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.data.*;
-
 import views.html.*;
-
 import models.*;
 
 public class Application extends Controller {
@@ -18,15 +16,36 @@ public class Application extends Controller {
     	return ok(show.render("Articles","Coucou"));
     }
     
+    
+  //Displays the front page
+    public static Result frontPage(){
+    	return ok(frontPage.render());
+    }
+    
+    
+    /* Authentication Management
+     * 
+     */
+    
     //Displays authentication page
     public static Result authenticationPage(){
     	return ok(authenticationPage.render());
     }
     
-    //Displays the front page
-    public static Result frontPage(){
-    	return ok(frontPage.render());
-    }
+    //Authentication method
+    public static Result authenticate(){  
+		Form<User> userForm = Form.form(User.class).bindFromRequest(); 
+		if(!userForm.hasErrors()){
+			session().clear();  
+			session("login", userForm.get().login);  
+            return redirect(routes.Application.frontPage());
+        }
+		else{
+			return badRequest(authenticationPage.render());
+		}
+	}
+    
+    
     
     //Displays Article Manager (Temporary)
     public static Result articlesManager(){
--- Articles/app/models/User.java
+++ Articles/app/models/User.java
@@ -1,12 +1,16 @@
 package models;
 
 import java.util.*;
+
 import play.modules.mongodb.jackson.MongoDB;
 import net.vz.mongodb.jackson.*;
+import play.data.format.*;
+import play.data.validation.*;
+
 import org.codehaus.jackson.annotate.JsonProperty;
 
 
-@MongoCollection(name="users")
+@MongoCollection(name="User")
 public class User{
 	@Id
 	@ObjectId
@@ -25,18 +29,69 @@ public class User{
 	public String role; // = "Visitor" or "Member" or "Supervisor" or "Admin" (for now)
 	
 	private static JacksonDBCollection<User, String> coll = MongoDB.getCollection("users", User.class, String.class);
-
+	
+	//Attribute Access methods
+	public String getLogin(){
+		return login;
+	}
+	
+	/*Methods allowing DataBase Management
+	 * 
+	 */
+	
+	// Method to collect all users from DataBase
 	public static List<User> all() {
 	    return coll.find().toArray();
 	}
 
+	// Method to add an user to the DataBase
 	public static void create(User user) {
 		coll.save(user);
 	}
 
+	// Method to delete an user from the DataBase
 	public static void delete(String id) {
 	    User user = User.coll.findOneById(id);
 	    if (user != null)
 	        coll.remove(user);
 	}
+	
+	
+	
+	/*Methods allowing Authentication
+	 * 
+	 */
+	
+	//Method to get a user by his login
+	public static User getByLogin(String loginEntered){
+		
+		
+		System.out.println(loginEntered);
+		
+		for(User x : coll.find().toArray()){
+			
+			System.out.println(x.getLogin().equals(loginEntered));
+			
+			if(x.getLogin().equals(loginEntered)){
+				System.out.println("Ca marche");
+				return x;
+			}
+		}
+		
+		return null;
+	}
+	
+	// Method for login validation
+	public String validate(){
+		User u = this.getByLogin(login);
+		if(u!=null){
+		if(login.equals(getByLogin(login).login)  &&   passw.equals(getByLogin(login).passw))  
+			return null;  
+		else
+			return  "Invalid login  or password";  
+		}
+		else
+			return  "Invalid login  or password";
+	}
+	
 }
